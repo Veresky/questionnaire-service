d1e404487093e9a3e7b0eb6d3b56cf102a79b5a2
Veresky <feng43542@gmail.com>
2024-01-14 01:57:14 -0500

    x

--- src/main/java/com/xiaohudu/questionnaire/config/MyWebMvcConfigurer.java
+++ src/main/java/com/xiaohudu/questionnaire/config/MyWebMvcConfigurer.java
@@ -4,6 +4,7 @@ import com.xiaohudu.questionnaire.interceptor.LoginInterceptor;
 import com.xiaohudu.questionnaire.interceptor.MonitorLogInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,6 +17,18 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
   @Bean
   public MonitorLogInterceptor monitorLogInterceptor() {
     return new MonitorLogInterceptor();
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+            //是否发送Cookie
+            .allowCredentials(true)
+            //放行哪些原始域
+            .allowedOrigins("*")
+            .allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"})
+            .allowedHeaders("*")
+            .exposedHeaders("*");
   }
 
   @Override
